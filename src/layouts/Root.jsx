11790c5983c11e9b6f200a1d7d6d3075902c1a70
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-05-14 14:17:47 +0600

    scroll linked animation style on root page using Framer Motion

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,11 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../pages/shared/Navbar/Navbar";
 import Footer from "../pages/shared/Footer/Footer";
-
+import { motion, useScroll } from "framer-motion";
+import "./Root.css";
 const Root = () => {
+  const { scrollYProgress } = useScroll();
   return (
     <div className="font-Jakarta relative">
-      <div className="sticky top-0 z-20 ">
+      <motion.div
+        className="progress-bar"
+        style={{ scaleX: scrollYProgress }}
+      ></motion.div>
+      <div className="sticky top-0 z-10 ">
         <Navbar />
       </div>
       <Outlet />
